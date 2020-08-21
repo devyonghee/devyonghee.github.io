b97265d92636d72dea11b58720b31ea614bb85f4
devyonghee <devyonghee@gmail.com>
2020-08-21 19:34:24 +0900

    change data

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://devyonghee.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Devyonghee Blog`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Blog posted about devyonghee`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
